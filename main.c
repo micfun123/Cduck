f4eb3baeade12ddc2cabcd61e92467e37acc19b5
micfun123 <54761507+micfun123@users.noreply.github.com>
2022-09-20 19:47:32 +0100

    quacj

--- main.c
+++ main.c
@@ -1,11 +1,22 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
 char tempvar[105];
 
+
 int main(){
+    srand(time(NULL));
     while (1)
     {
-       scanf("%s", tempvar);
-        printf("quack\n");
+        int r = rand() % 4 + 1;
+        scanf("%s", tempvar);
+        for (size_t i = 0; i < r; i++)
+        {
+            printf("quack ");
+        }
+        
     }
+    printf("\n");
     
 }
